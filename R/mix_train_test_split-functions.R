8699083ba7a8536263f468f81bcd477415f95b85
ehsan-f <ehsan.faridifar@gmail.com>
2025-09-26 20:08:33 +0100

    Update to train/test split function

---
+++ R/mix_train_test_split-functions.R
@@ -0,0 +1,50 @@
+#' Create training index
+#'
+#' @description
+#' Creates a stratified train_index column using caret's createDataPartition.
+#'
+#' @param df Data frame to add index to (default: ds)
+#' @param target Name of the target variable column for stratified sampling (default: 't_')
+#' @param p Proportion of data to use for training (default: 0.7)
+#'
+#' @export
+mix_train_index <- function(df = ds, target = 't_', p = 0.7) {
+
+  #-- Packages
+  library(data.table)
+  library(caret)
+
+  #-- Convert to data.table
+  setDT(df)
+
+  #-- Create index
+  train_ids <- createDataPartition(as.factor(df[[target]]), p = p, list = F)
+  df[, train_index := 0L]
+  df[train_ids, train_index := 1L]
+
+  return(df)
+}
+
+
+#' Split data into training and test sets
+#'
+#' @description
+#' Splits a data frame into training and test sets using the train_index column.
+#'
+#' @param df Data frame with train_index column (default: ds)
+#'
+#' @export
+mix_train_test_split <- function(df = ds) {
+
+  #-- Packages
+  library(data.table)
+
+  #-- Convert to data.table
+  setDT(df)
+
+  #-- Split data
+  list(
+    train = df[train_index == 1],
+    test = df[train_index == 0]
+  )
+}
